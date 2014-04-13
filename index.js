31824ecf2ed5cd90b023e9385414187477785d2d
Devon Govett <devongovett@gmail.com>
2014-04-12 17:08:51 -0700

    Support ArrayBufferViews in BlobBuilder constructor

--- index.js
+++ index.js
@@ -27,13 +27,30 @@ var blobSupported = (function() {
 var blobBuilderSupported = BlobBuilder
   && BlobBuilder.prototype.append
   && BlobBuilder.prototype.getBlob;
-
+  
 function BlobBuilderConstructor(ary, options) {
   options = options || {};
 
   var bb = new BlobBuilder();
   for (var i = 0; i < ary.length; i++) {
-    bb.append(ary[i]);
+    var chunk = ary[i];
+    
+    // extract ArrayBuffers from ArrayBufferViews
+    if (chunk.buffer instanceof ArrayBuffer) {
+      var buf = chunk.buffer;
+      
+      // if this is a subarray, make a copy so we only
+      // include the subarray region from the underlying buffer
+      if (chunk.byteLength !== buf.byteLength) {
+        var copy = new Uint8Array(chunk.byteLength);
+        copy.set(new Uint8Array(buf, chunk.byteOffset, chunk.byteLength));
+        buf = copy.buffer;
+      }
+      
+      bb.append(buf);
+    } else {
+      bb.append(chunk);
+    }
   }
   return (options.type) ? bb.getBlob(options.type) : bb.getBlob();
 };
